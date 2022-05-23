f59218b696dfff97bd166e10a6cb1eaa47182a57
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-05-22 21:59:29 -0400

    user login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
-import { login } from "../features/auth/authSlice";
+import { login, reset } from "../features/auth/authSlice";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import Spinner from "ui/Spinner";
 
 function Login() {
 	const [formData, setFormData] = useState({
@@ -12,10 +15,23 @@ function Login() {
 	const { email, password } = formData;
 
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
 	const { user, isError, isLoading, isSuccess, message } = useSelector(
 		(state) => state.auth
 	);
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
+
+		if (isSuccess || user) {
+			navigate("/");
+		}
+
+		dispatch(reset());
+	}, [isError, isSuccess, user, message, navigate, dispatch]);
 
 	const onChange = (e) => {
 		setFormData((prevState) => ({
@@ -34,6 +50,10 @@ function Login() {
 
 		dispatch(login(userData));
 	};
+
+	if (isLoading) {
+		return <Spinner />;
+	}
 
 	return (
 		<>
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
 import { register, reset } from "features/auth/authSlice";
 import { useDispatch, useSelector } from "react-redux";
+import Spinner from "ui/Spinner";
 
 function Register() {
 	const [formData, setFormData] = useState({
@@ -55,6 +56,10 @@ function Register() {
 			dispatch(register(userData));
 		}
 	};
+
+	if (isLoading) {
+		return <Spinner />;
+	}
 
 	return (
 		<>
