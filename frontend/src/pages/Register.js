21555d84705fe64d811397a4c9c998ee70ce1d04
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-05-22 20:50:16 -0400

    Register User

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
+import { register, reset } from "features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function Register() {
 	const [formData, setFormData] = useState({
@@ -11,6 +14,25 @@ function Register() {
 	});
 
 	const { name, email, password, password2 } = formData;
+
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const { user, isError, isLoading, isSuccess, message } = useSelector(
+		(state) => state.auth
+	);
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
+
+		if (isSuccess || user) {
+			navigate("/");
+		}
+
+		dispatch(reset());
+	}, [isError, isSuccess, user, message, navigate, dispatch]);
 
 	const onChange = (e) => {
 		setFormData((prevState) => ({
@@ -24,6 +46,13 @@ function Register() {
 
 		if (password !== password2) {
 			toast.error("Passwords do not match");
+		} else {
+			const userData = {
+				name,
+				email,
+				password,
+			};
+			dispatch(register(userData));
 		}
 	};
 
