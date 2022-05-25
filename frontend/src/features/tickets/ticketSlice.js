b48aac0a6ac168ba105ca9cffe66ac15fe84fdb4
Jason Kellie <93837635+jsnkle@users.noreply.github.com>
2022-05-25 19:00:06 -0400

    Create Ticket Functionality

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -1,5 +1,6 @@
 import ticketService from "features/tickets/ticketService";
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import authService from "features/auth/authService";
 
 const initialState = {
 	tickets: [],
@@ -10,13 +11,78 @@ const initialState = {
 	message: "",
 };
 
+// Create new ticket
+export const createTicket = createAsyncThunk(
+	"tickets/create",
+	async (ticketData, thunkAPI) => {
+		try {
+			const token = thunkAPI.getState().auth.user.token;
+			return await ticketService.createTicket(ticketData, token);
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
+
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
+// Get user tickets
+export const getTickets = createAsyncThunk(
+	"tickets/getAll",
+	async (_, thunkAPI) => {
+		try {
+			const token = thunkAPI.getState().auth.user.token;
+			return await ticketService.getTickets(token);
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
+
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
 export const ticketSlice = createSlice({
 	name: "ticket",
 	initialState,
-	reducers: {
-		reset: (state) => initialState,
+	reducers: { reset: (state) => initialState },
+	extraReducers: (builder) => {
+		builder
+			.addCase(createTicket.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(createTicket.fulfilled, (state) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+			})
+			.addCase(createTicket.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+			})
+			.addCase(getTickets.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(getTickets.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.tickets = action.payload;
+			})
+			.addCase(getTickets.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+			});
 	},
-	extraReducers: (builder) => {},
 });
 
 export const { reset } = ticketSlice.actions;
